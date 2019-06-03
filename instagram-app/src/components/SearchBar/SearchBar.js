00c19dd477376c3da20cfe88a18e6c928361952d
Nicholas Pung <nicholaspung>
2019-06-03 13:47:44 -0700

    used reactstrap to setup styling for searchbar

--- instagram-app/src/components/SearchBar/SearchBar.js
+++ instagram-app/src/components/SearchBar/SearchBar.js
@@ -1,16 +1,17 @@
 import React from 'react';
+import { Container, Row, Col } from 'reactstrap';
 
 const SearchBar = () => {
     return (
-        <div>
-            <div>Logo</div>
-            <div>SearchBar</div>
-            <div>
-                <div>Explore</div>
-                <div>Heart + Tooltip</div>
-                <div>Profile</div>
-            </div>
-        </div>
+        <Container>
+            <Row>
+                <Col xs="3">Logo</Col>
+                <Col xs={{ size: 2, offset: 2 }}>SearchBar</Col>
+                <Col xs={{ size: 1, offset: 2 }}>Explore</Col>
+                <Col xs="1">Heart</Col>
+                <Col xs="1">Profile</Col>
+            </Row>
+        </Container>
     );
 }
 
